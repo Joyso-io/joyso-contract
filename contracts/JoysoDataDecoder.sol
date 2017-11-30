b653cfeecd9b2af0233b44aafe8cc6878e1dc154
willliw <flew80530@gmail.com>
2017-11-30 21:09:31 +0800

    add decoder and corrosponding test. (not pass the test yet.)

---
+++ contracts/JoysoDataDecoder.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.4.17;
+
+contract JoysoDataDecoder {
+
+    function decodeData1 (uint256 data) public constant returns (bool isBuy, address token) {
+        /**
+            data1
+            0x00000000000000000000000 1 b2f7eb1f2c37645be61d73953035360e768d81e6
+            [23..23] (bool) isBuy
+            [24..63] (address) token 
+         */
+         token = (address)(data & 0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff);
+         isBuy = (data & 0x00000000000000000000000f0000000000000000000000000000000000000000) > 0;
+    }
+
+    function decodeData2 (uint256 _data) public constant returns (uint256 txFee, uint256 timestamp, uint256 joyPrice, address userAddress) {
+        /**
+            data2
+            0x00 0014 160004a1170 0000000 b2f7eb1f2c37645be61d73953035360e768d81e6
+            [2 .. 5] (uint256) txFee
+            [6 ..16] (uint256) timestamp
+            [17..23] (uint256) joyPrice --> 0: pay ether, others: pay joy Token
+            [24..63] (address) userAddress
+         */
+        // Assume the _data is come after retriveV, which already eliminated the first two bytes.  
+        txFee = _data / 0x000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+        _data = _data & 0x000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+        timestamp = _data / 0x00000000000000000fffffffffffffffffffffffffffffffffffffffffffffff;
+        _data = _data & 0x00000000000000000fffffffffffffffffffffffffffffffffffffffffffffff;
+        joyPrice = _data / 0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff;
+        userAddress = (address)(_data & 0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff);
+    }
+
+    function decodeData3 (uint256 _data) public constant returns (uint256 timestamp, uint256 joyPrice, uint256 paymentMethod, address token) {
+        /**
+            data3
+            0x00000 160004a1170 0000000 1 b2f7eb1f2c37645be61d73953035360e768d81e6
+            [5 ..15] (uint256) timestamp
+            [16..22] (uint256) joysoPrice
+            [23..23] (uint256) paymentMethod --> 0: ether, 1: Token, 2: joyToken
+            [24..63] (address) token
+         */
+        // Assume the _data is come after retriveV, which already eliminated the first two bytes.  
+        timestamp = _data / 0x0000000000000000ffffffffffffffffffffffffffffffffffffffffffffffff;
+        _data = _data & 0x0000000000000000ffffffffffffffffffffffffffffffffffffffffffffffff;
+        joyPrice = _data / 0x00000000000000000000000fffffffffffffffffffffffffffffffffffffffff;
+        _data = _data & 0x00000000000000000000000fffffffffffffffffffffffffffffffffffffffff;
+        paymentMethod = _data / 0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff;
+        token = (address)(_data & 0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff);
+    }
+
+    function decodeData4 (uint256 _data) public constant returns (uint256 timestamp, address userAddress) {
+        /**
+            data4 
+            0x0000000000000 160004a1170 b2f7eb1f2c37645be61d73953035360e768d81e6
+            [13..23] (uint256) timestamp
+            [24..63] (address) userAddress
+         */
+        // Assume the _data is come after retriveV, which already eliminated the first two bytes.  
+        timestamp = _data / 0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff;
+        userAddress = (address)(_data & 0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff);
+    }
+
+    function retrieveV (uint256 _data) public constant returns (uint256 data, uint256 v) {
+        v = _data / 0x00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+        v = _data / 0x0100000000000000000000000000000000000000000000000000000000000000;
+        data = _data & 0x00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+    }
+}
---
+++ test/TestDecoder.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.4.17;
+import "../contracts/JoysoDataDecoder.sol";
+import "truffle/Assert.sol";
+
+contract TestDecoder {
+    uint256 constant data1 = 0x000000000000000000000001b2f7eb1f2c37645be61d73953035360e768d81e6;
+    uint256 constant data2V = 0x1c0014160004a11703000000b2f7eb1f2c37645be61d73953035360e768d81e6;
+    uint256 constant data3V = 0x1c000160004a117030000001b2f7eb1f2c37645be61d73953035360e768d81e6;
+    uint256 constant data4V = 0x1c00000000001160004a1170b2f7eb1f2c37645be61d73953035360e768d81e6;
+
+    function testDecodeData1 () public {
+        JoysoDataDecoder aa = new JoysoDataDecoder();
+        bool isBuy;
+        address token;
+        (isBuy, token) = aa.decodeData1(data1);
+        Assert.equal(isBuy, true, "isBuy should be true");
+        Assert.equal(token, 0xb2f7eb1f2c37645be61d73953035360e768d81e6, "token address should be 0xb2f7eb1f2c37645be61d73953035360e768d81e6");
+    }
+
+    function testDecodeData2 () public {
+        JoysoDataDecoder aa = new JoysoDataDecoder();
+        uint256 v_256;
+        uint256 data;
+        uint256 txFee;
+        uint256 timeStamp;
+        uint256 joyPrice;
+        address userAddress;
+        (v_256, data) = aa.retrieveV(data2V);
+        Assert.equal(v_256, 0x1c, "v should be 1c");
+        (txFee, timeStamp, joyPrice, userAddress) = aa.decodeData2(data);
+        Assert.equal(txFee, 0x0014, "txFee should be 14");
+        Assert.equal(timeStamp, 0x160004a1170, "timeStamp should be 160004a1170");
+        Assert.equal(joyPrice, 0x3000000, "joyPrice should be 0x3000000");
+        Assert.equal(userAddress, 0xb2f7eb1f2c37645be61d73953035360e768d81e6, "userAddress should be 0xb2f7eb1f2c37645be61d73953035360e768d81e6");
+    }
+
+    function testDecodeData3 () public {
+        JoysoDataDecoder aa = new JoysoDataDecoder();
+        uint256 v_256;
+        uint256 data;
+        uint256 timeStamp;
+        uint256 joyPrice;
+        uint256 paymentMethod;
+        address token;
+        (v_256, data) = aa.retrieveV(data3V);
+        Assert.equal(v_256, 0x1c, "v should be 1c");
+        (timeStamp, joyPrice, paymentMethod, token) = aa.decodeData3(data);
+        Assert.equal(timeStamp, 0x160004a1170, "timeStamp should be 160004a1170");
+        Assert.equal(joyPrice, 0x3000000, "joyPrice should be 0x3000000");
+        Assert.equal(paymentMethod, 0x1, "paymentMethod should be 1");
+        Assert.equal(token, 0xb2f7eb1f2c37645be61d73953035360e768d81e6, "token address should be 0xb2f7eb1f2c37645be61d73953035360e768d81e6");
+    }
+
+    function testDecodeData4 () public {
+        JoysoDataDecoder aa = new JoysoDataDecoder();
+        uint256 v_256;
+        uint256 data;
+        uint256 timeStamp;
+        address userAddress;
+        (v_256, data) = aa.retrieveV(data4V);
+        Assert.equal(v_256, 0x1c, "v should be 1c");
+        (timeStamp, userAddress) = aa.decodeData4(data); 
+        Assert.equal(timeStamp, 0x160004a1170, "timeStamp should be 160004a1170");
+        Assert.equal(userAddress, 0xb2f7eb1f2c37645be61d73953035360e768d81e6, "userAddress should be 0xb2f7eb1f2c37645be61d73953035360e768d81e6");
+    }
+}
