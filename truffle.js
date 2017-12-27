82a0072dda97225c9644f1e64a5dd80d0d0c3c7d
willliw <flew80530@gmail.com>
2017-12-27 13:49:39 +0800

    add compiler information in truffle.js

--- truffle.js
+++ truffle.js
@@ -1,9 +1,15 @@
 module.exports = {
-    networks: {
-      development: {
-        host: "localhost",
-        port: 8545,
-        network_id: "*" // Match any network id
-      }
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
-  };
+  },
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
